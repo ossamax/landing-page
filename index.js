36e6a0b3a5054cf85c5bc8f50aaa664cd2ba4350
ossama zaoute <ramziioussama@gmail.com>
2021-05-29 18:58:28 +0100

    update

---
+++ index.js
@@ -0,0 +1,49 @@
+var Btns = $(".btn");
+var content = $(".imgs");
+
+for (let i = 0; i <= Btns.length; i++) {
+  Btns.eq(i).on("click", () => {
+    content.removeClass("active");
+    Btns.removeClass("active");
+    content.eq(i).addClass("active");
+    Btns.eq(i).addClass("active");
+  });
+}
+
+const itemsWrapper = document.querySelector(".items_wrapper");
+const carsoulimgs = document.querySelectorAll(".item");
+const leftArrow = document.querySelector(".left_arrow");
+const rightArrow = document.querySelector(".right_arrow");
+
+const slider = Array.from(itemsWrapper.children);
+const sliderWidth = slider[0].getBoundingClientRect().width;
+
+slider.forEach((slide, index) => {
+  slide.style.left = (sliderWidth + 40) * index + "px";
+});
+
+let counter = 0;
+let size = carsoulimgs[0].clientWidth + 40;
+itemsWrapper.style.transform = "translateX(" + -size * counter + "px)";
+
+console.log(counter);
+//move to the left
+leftArrow.addEventListener("click", () => {
+  counter--;
+  itemsWrapper.style.transform = "translateX(" + -size * counter + "px)";
+  console.log(counter);
+});
+
+//move to the right
+rightArrow.addEventListener("click", () => {
+  counter++;
+  itemsWrapper.style.transform = "translateX(" + -size * counter + "px)";
+  console.log(counter);
+});
+
+itemsWrapper.addEventListener("transitionend", () => {
+  if (carsoulimgs[counter].id === "lastclone") {
+    counter = carsoulimgs.length - 1;
+    itemsWrapper.style.transform = "translateX(" + -size * counter + "px)";
+  }
+});
